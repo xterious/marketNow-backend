eb2324d18ac2af9379d664887aba88f47c68bfa1
amar-chan <119720629+amar-chan@users.noreply.github.com>
2025-06-29 03:29:00 +0530

    updated corsconfig

--- src/main/java/com/marketview/Spring/MV/config/CorsConfig.java
+++ src/main/java/com/marketview/Spring/MV/config/CorsConfig.java
@@ -1,16 +1,25 @@
 package com.marketview.Spring.MV.config;
 
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/")  // Allow all paths
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080") // Add your WebSocket origin
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8082",
+                        "http://localhost:5173",
+                        "http://localhost:3001"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
